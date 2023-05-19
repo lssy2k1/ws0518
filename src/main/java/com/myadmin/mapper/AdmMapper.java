2070e9666ee478e9f7082aaa943375becd91ab6b
lssy2k1 <lssy2k1@gmail.com>
2023-05-19 09:09:44 +0900

    Initial commit

---
+++ src/main/java/com/myadmin/mapper/AdmMapper.java
@@ -0,0 +1,12 @@
+package com.myadmin.mapper;
+
+import com.myadmin.dto.Adm;
+import com.myadmin.dto.Lecture;
+import com.myadmin.frame.MyAdminMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface AdmMapper extends MyAdminMapper<String, Adm> {
+}
